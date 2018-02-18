32f328f2d400363d6555ae6cbcd8f15803117a94
Steve Sargent <ssarge70@gmail.com>
2018-02-17 18:02:57 -0800

    Initial commit with bolierplate

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize-css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>I was here</p>, document.getElementById('app'));
