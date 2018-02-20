9c7b005ec5749570c954aacbd78041c6111793f7
Steve Sargent <ssarge70@gmail.com>
2018-02-20 06:11:25 -0800

    Add date range pickers

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,37 +1,63 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setTextFilter,sortByAmount,sortByDate} from '../actions/filters';
+import {setTextFilter,sortByAmount,sortByDate, setStartDate, setEndDate} from '../actions/filters';
+import 'react-dates/initialize';
+import {DateRangePicker} from 'react-dates';
 
-const ExpenseListFilters = (props) => (
-    <div>
-        <div>
-            <label htmlFor="filterTextBox">Add a text filter: </label>
-            <input type="text" 
-                value={props.filters.text} 
-                name="filterTextBox" 
-                placeholder="Add text here..."
-                onChange={(e) => {
-                    props.dispatch(setTextFilter(e.target.value));
-                }} />
-        </div>
-        <div>
-            <label htmlFor = "sortByDropdown">Sort by: </label>
-            <select
-                value={props.filters.sortBy}
-                onChange={(e) =>{
-                    if(e.target.value === 'date'){
-                        props.dispatch(sortByDate());
-                    }else if(e.target.value === 'amount'){
-                        props.dispatch(sortByAmount());
-                    }
-                }}
-            >
-                <option value="date">Date</option>
-                <option value="amount">Amount</option>
-            </select>
-        </div>
-    </div>
-);
+class ExpenseListFilters extends React.Component{
+    
+    state = {
+        calendarFocused: null
+    };
+
+    onDatesChange = ({startDate, endDate}) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    };
+
+    onFocusChange = ((calendarFocused) => {
+        this.setState(() => ({calendarFocused}));
+    })
+
+    render(){
+        return (
+            <div>
+                <div>
+                    <input type="text" 
+                        value={this.props.filters.text} 
+                        name="filterTextBox" 
+                        placeholder="Add text filter here..."
+                        onChange={(e) => {
+                            this.props.dispatch(setTextFilter(e.target.value));
+                        }} />
+                    <select
+                        value={this.props.filters.sortBy}
+                        onChange={(e) =>{
+                            if(e.target.value === 'date'){
+                                this.props.dispatch(sortByDate());
+                            }else if(e.target.value === 'amount'){
+                                this.props.dispatch(sortByAmount());
+                            }
+                        }}
+                    >
+                        <option value="date">Date</option>
+                        <option value="amount">Amount</option>
+                    </select>
+                        <DateRangePicker 
+                            startDate={this.props.filters.startDate}
+                            endDate={this.props.filters.endDate}
+                            onDatesChange={this.onDatesChange}
+                            focusedInput={this.state.calendarFocused}
+                            onFocusChange={this.onFocusChange}
+                            showClearDates={true}
+                            numberOfMonths={1}
+                            isOutsideRange={() => false}
+                        />
+                </div>
+            </div>
+        );
+    }
+}
 
 const connectMap = (state) => {
     if(true){
--- src/store/expenses-store.js
+++ src/store/expenses-store.js
@@ -6,6 +6,6 @@ export default () => {
     const store = createStore(combineReducers({
         expenses:expensesReducer,
         filters:filtersReducer
-    }));
+    }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
     return store;
 };
