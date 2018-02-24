922956851aa664a733901175bfd2be31b317deeb
Steve Sargent <ssarge70@gmail.com>
2018-02-24 14:33:45 -0800

    Express server

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,7 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/add" activeClassName="is-active" exact={true}>Add Expense Page</NavLink>
-        <NavLink to="/help" activeClassName="is-active" exact={true}>Help Page</NavLink>
+        <NavLink to="/add" activeClassName="is-active" exact={true}>Create Expense</NavLink>
     </header>
 );
 
