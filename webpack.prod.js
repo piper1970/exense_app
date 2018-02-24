8e30f996f3ddf6ed453ada3e16664b2b3b8ec453
Steve Sargent <ssarge70@gmail.com>
2018-02-24 09:56:48 -0800

    Restore webpack.config.js and remove webpack.{dev|prod}.js

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const merge = require('merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.common');
-
-module.exports = merge(common, {
-    plugins: [
-        new UglifyJSPlugin()
-    ]
-});
